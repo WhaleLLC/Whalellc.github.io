5ff94e414892b8b2003aac7e4f1e6c6d0a8fd0f1
Denis Kalinichenko <denis.kalinichenko@outlook.com>
2015-09-17 19:39:40 +0300

    Улучшение в тексте заявки и позиционировании фона

--- form.php
+++ form.php
@@ -3,7 +3,7 @@ if($_POST){
   $to = "instarlpc@gmail.com";
   $from = 'instarlpc@gmail.com';
   $subject = "Проверка почты";
-  $message = 'Имя: ' . $_POST['name'] . '/n' . 'Телефон: ' . $_POST['phone'];
+  $message = 'Имя: ' . $_POST['name'] . '\n' . 'Телефон: ' . $_POST['phone'];
   $headers = "Content-type: text/html; charset=UTF-8 \r\n";
   $headers .= "From: <instarlpc@gmail.com>\r\n";
   $result = mail($to, $subject, $message, $headers);
